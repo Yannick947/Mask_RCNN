f818e3766f1bd04de6f31d2b6574a3fa0b1eeb14
Yannick947 <yannick.schrempp@web.de>
2020-12-03 12:15:19 +0100

    Init debugger

---
+++ tests/debug_data_generator.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+
+# Activate free gpu
+# import nvgpu
+
+# available_gpus = nvgpu.available_gpus()
+
+# if type(available_gpus) is list and len(available_gpus) > 0:
+#     os.environ["CUDA_VISIBLE_DEVICES"] = available_gpus[0]
+#     print('Using GPU ', available_gpus[0])
+
+# else: 
+#     print('No free gpu found, try later..')
+#     exit()
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath('./')
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  
+from mrcnn.config import Config
+from mrcnn.model import data_generator 
+from samples.sunrgbd.sun import SunConfig, SunDataset
+
+CLASSES = ['bed', 'tool', 'desk', 'chair', 'table', 'wardrobe', 'sofa', 'bookcase']
+ANNOTATION_FILENAME = 'via_regions_sunrgbd.json'
+CVHCI_DATASET_PATH = "/cvhci/data/depth/SUNRGBD/"
+LOCAL_PATH_DATASET = "C:/Users/Yannick/Downloads/SUNRGBD"
+
+IGNORE_IMAGES_PATH = os.path.join(LOCAL_PATH_DATASET, 'skip_image_paths.txt')
+DEBUG_DATASET = "train" # Can be 'train', 'val' or 'test
+
+def main():
+    dataset = SunDataset(SunConfig())
+    dataset.load_sun(LOCAL_PATH_DATASET, DEBUG_DATASET)
+    dataset.prepare()
+    gen = data_generator(dataset, SunConfig() , shuffle=True,
+                                              augmentation=False,
+                                              batch_size=2)
+
+    while True:
+        next(gen)
+
+if __name__ == '__main__':
+    main()
